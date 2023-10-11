fcce23424332fb130af6a8311b28bb90504d4310
MINHPHUONG1409 <nguyenminhphuong1409@gmail.com>
2023-10-11 23:00:57 +0700

    CellphoneS

---
+++ CellPhoneS/Detail.js
@@ -0,0 +1,42 @@
+﻿let list = document.querySelector('.slider .list')
+let item = document.querySelectorAll('.slider .list .item')
+let dots = document.querySelectorAll('.slider .dots li')
+let prev = document.getElementById('prev')
+let next = document.getElementById('next')
+
+let active = 0;
+let lengthItem = item.length - 1;
+next.onclick = function () {
+    if (active + 1 > item.length) {
+        active = 0;
+    }
+    else {
+        active = active + 1;
+    }
+    reloadSlider();
+}
+prev.onclick = function () {
+    if (active - 1 < 0) {
+        active = lengthItem;
+    } else {
+        active = active - 1;
+    }
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(() => {next.click(),5000})
+function reloadSlider() {
+    let CheckLeft = item[active].offsetLeft 
+    list.style.left = - CheckLeft + 'px';
+
+    let LastActiveDot = document.querySelector('.slider .dots li.active');
+    LastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+}
+dots.forEach((li, key) => {
+    li.addEventListener('click', function () {
+        active = key;
+        reloadSlider();
+    })
+})
---
+++ CellPhoneS/ProductJVS.js
@@ -0,0 +1,11 @@
+﻿const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            checkbox.nextElementSibling.style.backgroundColor = '#bdc3c7';
+        } else {
+            checkbox.nextElementSibling.style.backgroundColor = 'transparent';
+        }
+    });
+});
